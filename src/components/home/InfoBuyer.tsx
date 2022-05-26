bee575d2bb40c6415536669477350f93988568a6
Quocdat1205 <quocdatsc1205@gmail.com>
2022-05-26 11:02:04 +0700

    add display price ETH

--- src/components/home/InfoBuyer.tsx
+++ src/components/home/InfoBuyer.tsx
@@ -4,6 +4,7 @@ import { useContract } from "@hook/useContract";
 
 const InfoBuyer = () => {
   const { infoBuyer } = useContract();
+  console.log(infoBuyer);
 
   return (
     <Box bg="gray" width="40%" margin="auto" padding="1rem">
@@ -11,7 +12,8 @@ const InfoBuyer = () => {
       <Text>Address: {infoBuyer[0]}</Text>
       <Text>Amout: {infoBuyer[1]}</Text>
       <Text>Expired: {infoBuyer[2]}</Text>
-      <Text>{`State: ${infoBuyer[3]}`}</Text>
+      <Text>{`State: ${infoBuyer[4]}`}</Text>
+      <Text>Price ETH: {infoBuyer[3]}$</Text>
     </Box>
   );
 };
